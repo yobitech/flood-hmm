6b1195e0a929437b5fdbd2f9272c3c00d94a311c
jming <jming@college.harvard.edu>
2016-03-30 16:36:13 +0530

    clean up pipeline

--- pipeline.py
+++ pipeline.py
@@ -32,15 +32,16 @@ def pipeline():
 	############################
 
 	# 2. format data
-	ws_df = pd.DataFrame(ws_data)
-	aw_df = pd.DataFrame(aw_data)
-
-		# end with: 
+			# end with: 
 		# yobi: date, measured value
 			# sum/average all of the rainfalls for a given day
 			# condense the date to the given day
 		# accuweather: date, days ahead, rf, pct
 		# make sure that the last measured days are the same!!!
+
+	ws_df = format_data(ws_data, 'ws')
+	aw_df = format_data(aw_data, 'aw', 
+		params={'measurements': measurements, 'n_predict': n_predict, 'n_times': n_times})
 
 	############################
 
@@ -79,10 +80,82 @@ def get_data(source, columns, n_rows):
 
 	return results
 
-def get_col(source, range, n_predict=-1):
+def format_data(source, dtype, params={}):
+
+	if dtype == 'ws':
+
+		# consolidate rainfalls for rows so that it is one row per day
+		data = {'date': [], 'rainfall': []}
+
+		current_date = None
+		current_rf = 0.
+
+		for row in source:
+			# TODO: take only the 'date' portion of the last_updated
+			date = row['last_updated']
+			if not current_date:
+				current_date = date
+				current_rf = row['rainfall']
+			else:
+				# TODO: check to see if we need to sum rainfalls
+				if current_date == date:
+					current_rf += row['rainfall']
+				else:
+					data['date'].append(current_date)
+					data['rainfall'].append(current_rf)
+					current_date = date
+					current_rf = row['rainfall']
+
+		return data
+
+	elif dtype == 'aw':
+
+		data = {}
+		for day in range(1, params['n_predict']+1):
+			data[day] = {'date': []}
+			for measurement in params['measurements']:
+				data[measurement] = []
+
+		for row in source:
+			for i in range(1, params['n_predict']+1):
+				data[i]['date'].append(row['last_updated'])
+				for measurement in params['measurements']:
+					measurement_val = 0.
+					for j in range(1, params['n_times']+1):
+						measurement_val += row[str(i) + 'aw' + measurement + str(j)]
+				data[i][measurement].append(measurement_val/params['n_times'])
+
+
+		# data = {'date': [], 'days_ahead': []}
+		# for measurement in params['measurements']:
+		# 	data[measurement] = []
+
+		# # take all of the data and just flatten it (averaging over same day)
+		# for row in source:
+		# 	for i in range(1, params['n_predict']+1):
+		# 		# TODO: take only the 'date' portion of last_updated
+		# 		data['date'].append(row['last_updated'])
+		# 		data['days_ahead'].append(i)
+		# 		for measurement in params['measurements']: 
+		# 			measurement_val = 0.
+		# 			for j in range(1, params['n_times']+1):
+		# 				measurement_val += row[str(i) + 'aw' + measurement + str(j)]
+		# 			# TODO: do we want the average of the measurements?
+		# 			data[measurement].append(measurement_val/params['n_times'])
+
+		return data
+
+def get_col(source, crange, n_predict=-1):
+
+	start,end = crange
+
+	if n_predict > 0:
+		return np.array(source[n_predict]['rf'][start:end])
+	else:
+		return np.array(source['rainfall'][start:end])
 
 	# return numpy array
-	pass
+	# pass
 
 
 def calc_r2(actual, predicted):
